b1400788a6974a3713fd01848f61dd879eab5f7c
Maviooo <marolojek@gmail.com>
2023-03-21 00:07:39 +0100

    Made simple connection from multiple lients at once

--- client_mockup.py
+++ client_mockup.py
@@ -1,8 +1,10 @@
 import socket
+import sys
+from time import sleep
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
 
-clientSocket.connect(("127.0.0.1",6960));
+clientSocket.connect(("127.0.0.1", int(sys.argv[1])));
 
 data = "Hello Server!";
 
@@ -10,4 +12,7 @@ clientSocket.send(data.encode());
 
 dataFromServer = clientSocket.recv(1024);
 print(dataFromServer.decode());
-
+while True:
+    input("Press Enter to continue...")
+    clientSocket.send("Button pressed!!!".encode());
+clientSocket.close()
--- server.py
+++ server.py
@@ -1,23 +1,40 @@
 #!/usr/bin/python3
+
 import socket
 import sys
 import threading
+from time import sleep
 
-def on_new_client(clientsocket,addr):
+clients_ids = []
+
+def on_new_client(clientsocket, addr):
+    msg = clientsocket.recv(1024)
+    print (addr, " >> ", msg.decode())
+    clients_ids.append(addr)
+    clientsocket.send(f"You are user {clients_ids.index(addr)}".encode())
     while True:
-        msg = clientsocket.recv(1024)
-        #do some checks and if msg == someWeirdSignal: break:
-        print (addr, " >> ", msg)
-        #Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
-        clientsocket.send(msg)
+        print(wait_on_press(clientsocket, addr))
+        send_id_to_all()
+        print("Ready for new button press")
     clientsocket.close()
+
+def wait_on_press(clientsocket, addr):
+    while(clientsocket.recv(1024)):
+        sock.setblocking(1)
+        return (f"Button pressed id {clients_ids.index(addr)}")
+    
+
+def send_id_to_all():
+    sleep(5)
+    sock.setblocking(0)
+    return
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('localhost', 6960)
-print(sys.stderr, 'starting up on %s port %s' % server_address)
+server_address = ('localhost', int(sys.argv[1]))
+print('Starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
 # Listen for incoming connections
